cb50a8bb8acc7a2ee8b385a66639dff84ee556ba
Jean Luc Houédanou <jhouedanou@gmail.com>
2025-11-11 20:30:20 +0000

    Refactor code structure for improved readability and maintainability

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -58,7 +58,7 @@ export default defineNuxtConfig({
   // Nitro Configuration (Server)
   nitro: {
     compressPublicAssets: true,
-    preset: 'netlify-static',
+    preset: 'vercel',
     prerender: {
       crawlLinks: false,
       routes: ['/', '/about', '/services', '/contact', '/products', '/news', '/faq', '/product-category']
